0868f2093e29f1c6577fee6a0e01ca4e120f0fd4
Angel-Celaya <angel.celayagrc@uanl.edu.mx>
2021-10-26 08:36:20 -0500

    FECHA CORRECTA EN EL WORD

---
+++ API/Con_Api.sh
@@ -0,0 +1,14 @@
+#API para  obtener un HTTP request
+curl https://api.nasa.gov/planetary/apod?api_key=tDo3PMCibbRADHbmKWWvTaExQWlnUaa16RcT9ir2
+
+#API para obtener una imagen satelital de la tierra
+curl https://api.nasa.gov/planetary/earth/imagery?api_key=tDo3PMCibbRADHbmKWWvTaExQWlnUaa16RcT9ir2
+
+#API para acceder al TECHPORT de la NASA
+curl https://api.nasa.gov/techport/api/projects/17792?api_key=tDo3PMCibbRADHbmKWWvTaExQWlnUaa16RcT9ir2
+
+#API para obtener  la TLE API de la NASA
+curl http://tle.ivanstanojevic.me
+
+#API  para acceder a la libreria de imagen y video de la NASA
+curl https://images-api.nasa.gov
